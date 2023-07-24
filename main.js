13432f4dc6c5193a4bff48ae26dd257e26be41d3
Francisco Mena <115848687+Isco83@users.noreply.github.com>
2023-07-24 17:01:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function ageCalculator(){
+
+const diaNacimiento = parseInt(document.getElementById('day').value);
+const mesNacimiento = parseInt(document.getElementById('mounht').value);
+const añoNacimiento = parseInt(document.getElementById('year').value)
+
+
+}
