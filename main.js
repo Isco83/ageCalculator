b5da1f6ee724070161036b21199cf15d964b7acc
Francisco Mena <115848687+Isco83@users.noreply.github.com>
2023-07-24 21:12:14 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,27 @@
-function ageCalculator(){
+//crear Variable para imputs e Identificar 
+const diaNacimiento = document.getElementById('day');
+const mesNacimiento = document.getElementById('mounht');
+const añoNacimiento = document.getElementById('year');
 
-const diaNacimiento = parseInt(document.getElementById('day').value);
-const mesNacimiento = parseInt(document.getElementById('mounht').value);
-const añoNacimiento = parseInt(document.getElementById('year').value)
+//Crear Variable Para outputs e Identificar
+const diaNacimiento_out = document.getElementById('cajainput_DD');
+const mesNacimiento_out = document.getElementById('cajainput_MM');
+const añoNacimiento_out = document.getElementById('cajainput_YY');
+
+//crear variable para el form + obtener el primer elemento del documento
+const form = document.querySelector("form");
+
+//agregamos un evento listener a la forma
+form.addEventListener("submit",);
+
+const date = new Date();
+let day = date.getDate();
+let month = 1 + date.getMonth();
+let year = date.getFullYear();
+
+//declarando variables para meses de  30, 28, 31 y bisiestos 
+const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
 
-}
+
+
