ccdb049367135b592fb37955efd235026cc3f339
Francisco Mena <115848687+Isco83@users.noreply.github.com>
2023-08-10 19:46:40 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ const mesNacimiento = document.getElementById('mounht');
 const añoNacimiento = document.getElementById('year');
 
 //Crear Variable Para outputs e Identificar
-const diaNacimiento_out = document.getElementById('cajainput_DD');
-const mesNacimiento_out = document.getElementById('cajainput_MM');
-const añoNacimiento_out = document.getElementById('cajainput_YY');
+const diaNacimiento_out = document.getElementById('DD');
+const mesNacimiento_out = document.getElementById('MM');
+const añoNacimiento_out = document.getElementById('YY');
 
 //crear variable para el form + obtener el primer elemento del documento
 const form = document.querySelector("form");
@@ -22,6 +22,30 @@ let year = date.getFullYear();
 //declarando variables para meses de  30, 28, 31 y bisiestos 
 const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
+function validar(){
+    const inputs = document.querySelectorAll("input");
+    let validador = true;
+    inputs.forEach((i) =>{
+        const parent = i.parentElement;
+        if (!i.value) {
+            i.style.borderColor = "red";
+            parent.querySelector("letras").innerText = "Este Campo Es Requerido"
+            validador = false;
+        } else if (month.value > 12){
+            month.style.borderColor = "red";
+            month.parentElement.querySelector("letras").innerText = "Debe Ser Un Mes Valido"
+            validador = false;
+        } else if (day.value > 31){
+            day.style.borderColor = "red";
+            day.parentElement.querySelector("letras").innerText = "Debe Ser Un Dia Valido"
+            validador = false;
+        } else {
+            i.style.borderColor = "black";
+            parent.querySelector("letras").innerText = ""
+            validador = true; 
+        }
+    })
 
 
 
+}
